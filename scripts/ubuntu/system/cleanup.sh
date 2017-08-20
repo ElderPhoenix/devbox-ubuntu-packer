9baec5be60fa3aab8d479da228416ae8ecf8eb55
ElderPhoenix <vadimgnidashev@gmail.com>
2017-08-20 15:29:18 +0300

    Refactored scripts structure

--- scripts/ubuntu/system/cleanup.sh
+++ scripts/ubuntu/system/cleanup.sh
@@ -40,7 +40,7 @@ apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6;
 apt-get -y purge ppp pppconfig pppoeconf;
 
 # Delete oddities
-apt-get -y purge popularity-contest installation-report command-not-found command-not-found-data friendly-recovery bash-completion fonts-ubuntu-font-family-console laptop-detect;
+apt-get -y purge popularity-contest installation-report command-not-found command-not-found-data friendly-recovery fonts-ubuntu-font-family-console;
 
 # Delete the massive firmware packages
 apt-get -y purge linux-firmware
